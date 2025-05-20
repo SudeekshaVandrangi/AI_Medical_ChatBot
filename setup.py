9c3f86cc7a0db1aa786017bfc32c7b3c3378c383
SudeekshaVandrangi <shaindor09@gmail.com>
2025-05-20 15:47:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(
      name="Generative ai project",
      version="0.0.0",
-     author="Mohammed",
+     author="Sudeeksha",
      packages=find_packages(),
      install_requires = []
 )
